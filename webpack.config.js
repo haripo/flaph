6db3fb560a919d424c614e523d8c885c274aef80
haripo <haripo@users.noreply.github.com>
2019-07-28 16:33:09 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public/dist'),
+        publicPath: 'dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000
+    }
+};
