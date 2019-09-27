9eeea8bf22a2c96f3f289a479ba1e1832eb988de
haripo <haripo@users.noreply.github.com>
2019-09-27 16:43:46 +0000

    Automated deployment: Fri Sep 27 16:43:46 UTC 2019 ab7b0f93055b45173079c9afbcafed81bcd56757

--- react/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { createElement } from 'react';
-import { render } from 'react-dom';
-import Main from './Main';
-
-const domContainer = document.querySelector('#app');
-render(createElement(Main), domContainer);
--- vue/src/index.ts
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import Main from './Main.vue';
-
-new Vue({
-  render: (h) => h(Main)
-}).$mount('#app');
